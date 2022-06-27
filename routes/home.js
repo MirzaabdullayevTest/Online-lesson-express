8003f356d6fc8eaf34592a9a8aa1e089098c6c84
MirzaabdullayevDev <mirzaabdullayev023@gmail.com>
2022-06-27 19:54:31 +0500

    feat: pug ui

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,6 @@ const router = Router()
 
 // Get home page
 router.get('/', (req, res) => {
-    // res.send('<h1> Here is home page </h1>')
     res.render('index', {
         title: 'Home page'
     })
--- routes/lesson.js
+++ routes/lesson.js
@@ -1,15 +1,24 @@
 const express = require('express')
 const router = express.Router()
+const Joi = require('joi')
+const authMiddleware = require('../middleware/auth')
+const { v4: uuid } = require('uuid')
 
-const lessons = [
-    { name: 'Node js', id: 1 },
-    { name: 'React js', id: 2 },
-    { name: 'JavaScript', id: 3 },
-]
+const lessons = []
 
 // Get all lessons
 router.get('/', (req, res) => {
-    res.status(200).send(lessons)
+    res.render('lessons', {
+        title: 'All lessons',
+        lessons
+    })
+})
+
+// Get add lesson
+router.get('/add', (req, res) => {
+    res.render('addLesson', {
+        title: 'Add lesson'
+    })
 })
 
 // Get single lesson with name
@@ -17,6 +26,86 @@ router.get('/lesson', (req, res) => {
     // console.log(req.query);
     const lesson = lessons.find(les => les.name === req.query.name)
     res.status(200).send(lesson)
+})
+
+// Get single lesson with id
+router.get('/:id', (req, res) => {
+    // console.log(req.params); // bitta object
+    const lesson = lessons.find(les => les.id === req.params.id)
+    if (!lesson) {
+        return res.status(404).send('404 not found')
+    }
+    res.render("lesson", {
+        title: lesson.name,
+        lesson
+    })
+})
+
+// Delete single lesson with id
+router.get('/delete/:id', authMiddleware, (req, res) => {
+    const idx = lessons.findIndex(les => les.id === req.params.id)
+
+    // Validator
+    if (idx === -1) {
+        return res.status(404).send('404 not found. Id is not exist')
+    }
+
+    lessons.splice(idx, 1)
+    res.redirect('/api/lessons')
+})
+
+// Post add lesson
+router.post('/add', authMiddleware, (req, res) => {
+    const schema = Joi.object({
+        name: Joi.string().
+            min(3).
+            required(),
+        author: Joi.string()
+            .required(),
+        price: Joi.string()
+            .required(),
+        year: Joi.number(),
+        img: Joi.string()
+            .required()
+    })
+
+    const value = schema.validate(req.body)
+
+    if (value.error) {
+        res.status(404).send(value.error.message)
+        return
+    }
+
+    const lesson = {
+        name: req.body.name,
+        id: uuid(),
+        img: req.body.img,
+        year: req.body.year || new Date(),
+        author: req.body.author,
+        price: req.body.price
+    }
+
+    lessons.push(lesson)
+    res.redirect('/api/lessons')
+})
+
+// Put lesson with id
+router.put('/update/:id', authMiddleware, (req, res) => {
+    const idx = lessons.findIndex(les => les.id === +req.params.id)
+
+    // Validator
+    if (idx === -1) {
+        return res.status(404).send('404 not found. Id is not exist')
+    }
+
+    let lesson = {
+        name: req.body.name,
+        id: req.params.id
+    }
+
+    lessons[idx] = lesson
+
+    res.status(200).send('Lesson updated successfull')
 })
 
 module.exports = router
